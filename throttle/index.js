4ef82283f0933bb0870eef64fb6d60a87431f739
Matt Hillsdon <mth@corefiling.com>
2015-06-19 19:03:57 +0100

    Simplify

--- throttle/index.js
+++ throttle/index.js
@@ -3,24 +3,15 @@ function throttle(f, interval) {
   var timeout = null;
   return function() {
     var now = Date.now();
-    // Initial delay required by the tests.
     var delay = last === null ? interval : interval - (now - last);
-    if (delay <= 0) {
-      last = now;
-      f.apply(this, arguments);
-    }
-    else {
-      if (timeout !== null) {
-        clearTimeout(timeout);
-      }
-      var that = this;
-      var args = arguments;
-      timeout = setTimeout(function() {
-        timeout = null;
-        last = Date.now();
-        f.apply(that, args);
-      }, delay);
-    }
+    clearTimeout(timeout);
+    var that = this;
+    var args = arguments;
+    timeout = setTimeout(function() {
+      timeout = null;
+      last = Date.now();
+      f.apply(that, args);
+    }, delay);
   }
 }
 
